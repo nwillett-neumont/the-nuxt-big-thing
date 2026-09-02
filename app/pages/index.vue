96992d0128cbc3c2e870289921402561336045b1
Nate W. <nwillett@student.neumont.edu>
2026-09-02 17:29:36 -0600

    Updated subtext on main page.

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="text-white flex justify-center align-middle">
+  <div class="text-white flex flex-col text-center justify-center">
     <h1 class="text-8xl mt-8">Monster Tool</h1>
+    <p class="text-gray-500 mt-4">
+      The ultimate (terrible) Dungeons and Dragons monster viewing app.
+    </p>
   </div>
 </template>
